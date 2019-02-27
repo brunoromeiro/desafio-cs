cceb131a964d7d657d2fed917d822fee11a8b790
Bruno Romeiro <bruno.romeiro@gmail.com>
2019-02-27 12:59:34 -0300

    Adicionadas Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './shared/components/home/home.component';
+import { ReposComponent } from './shared/components/repos/repos.component';
+import { UserComponent } from './shared/components/user/user.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  { path: '', component: HomeComponent },
+  { path: 'repositorio/:name', component: ReposComponent },
+  { path: 'usuario/:user', component: UserComponent }
 ];
 
 @NgModule({
