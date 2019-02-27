02a23e78f3d2de7908326392cecadee25845b8c5
Bruno Romeiro <bruno.romeiro@gmail.com>
2019-02-27 12:51:52 -0300

    Adicionado User Component ao Shared Module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { ReposComponent } from './components/repos/repos.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    ReposComponent
+    ReposComponent,
+    UserComponent
   ],
   imports: [
     CommonModule
