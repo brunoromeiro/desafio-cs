b2631d060aab88a1caea7079639eb5420d48742b
Bruno Romeiro <bruno.romeiro@gmail.com>
2019-02-28 19:52:54 -0300

    Ajustes Home Component

--- src/app/shared/components/home/home.component.ts
+++ src/app/shared/components/home/home.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../../services/api.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
+
 export class HomeComponent implements OnInit {
+  datausername: string;
+  user: any;
+  conversao: any;
+  res;
+  data: any;
+  headElements = ['user'];
+  elements = {};
 
-  constructor() { }
-
+  constructor(private api: ApiService) { }
   ngOnInit() {
+    //code
   }
+
+  onSearch() {
+    console.log('cliquei', this.datausername);
+    this.api.searchUsers(this.datausername)
+      .subscribe(
+        data => {
+          this.res = {
+            data: data,
+            result: true
+          };
+          this.data = data;
+          console.log(this.elements);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+
+    console.log(this.user);
+  }
+
 
 }
