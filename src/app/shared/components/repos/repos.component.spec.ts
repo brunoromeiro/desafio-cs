27fa060782b52323ca3e32948828effe836318cc
Bruno Romeiro <bruno.romeiro@gmail.com>
2022-08-29 14:04:22 -0300

    build: atualiza angular para versão 11

--- src/app/shared/components/repos/repos.component.spec.ts
+++ src/app/shared/components/repos/repos.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ReposComponent } from './repos.component';
 
@@ -6,7 +6,7 @@ describe('ReposComponent', () => {
   let component: ReposComponent;
   let fixture: ComponentFixture<ReposComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ReposComponent ]
     })
